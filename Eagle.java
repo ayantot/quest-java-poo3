ae38117239006ab740fa81c7b88f2cc017a85cfa
TEGNY Thomas <thomas.tegny06@gmail.com>
2019-05-15 13:15:04 +0200

    Solution for my quest JAVA08 for WCS

--- Eagle.java
+++ Eagle.java
@@ -1,4 +1,4 @@
-public class Eagle extends Bird {
+public class Eagle extends Bird implements Fly {
 
     private boolean flying;
     private int altitude;
@@ -21,4 +21,43 @@ public class Eagle extends Bird {
     public String sing() {
         return "Screech!";
     }
+
+    
+    @Override 
+    public void takeOff(){
+        this.flying = true;
+        System.out.println(this.getName() + "takes off in the sky");
+    }
+
+    @Override 
+    public int ascend(int meters){
+        if (this.flying){
+            this.altitude += meters;
+        }
+        System.out.println(this.getName() + "flies upward, altitude : " + this.altitude);
+        return this.altitude;
+    }
+
+
+    @Override 
+    public int descend(int meters){
+        if (this.flying){
+            this.altitude -= meters;
+        }
+        System.out.println(this.getName() + "flies downward, altitude : " + this.altitude);
+        return this.altitude;
+    }
+
+    @Override 
+    public void land(){
+        if (this.altitude <=1){
+            this.flying = false;
+            this.altitude = 0;
+            System.out.println(this.getName() + "lands on the ground");
+        }
+        else{
+        System.out.println(this.getName() + " is too high, it can't lands");
+        }
+    }
+
 }
